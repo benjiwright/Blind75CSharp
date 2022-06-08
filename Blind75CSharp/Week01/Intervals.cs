b649fc80d60ef0649ca4958ad4ba562116f4a8b9
Benji <3194212+benjiwright@users.noreply.github.com>
2022-06-08 13:27:56 -0600

    intervals merge

---
+++ Blind75CSharp/Week01/Intervals.cs
@@ -0,0 +1,35 @@
+﻿namespace Blind75CSharp.Week01;
+
+public class Intervals
+{
+   public int[][] Merge(int[][] intervals)
+   {
+      intervals = intervals.OrderBy(x => x[0]).ToArray();
+
+      var results = new List<int[]>();
+
+      var start = intervals[0][0];
+      var end = intervals[0][1];
+
+      for (var i = 1; i < intervals.Length; i++)
+      {
+         if (end >= intervals[i][0])
+         {
+            end = Math.Max(end, intervals[i][1]);
+         }
+         else
+         {
+            results.Add(new[] {start, end});
+            start = intervals[i][0];
+            end = intervals[i][1];
+         }
+      }
+
+      results.Add(new[] {start, end});
+
+      return results.ToArray();
+   }
+}
+
+// Runtime: 226 ms, faster than 52.85% of C# online submissions for Merge Intervals.
+// Memory Usage: 47.1 MB, less than 43.34% of C# online submissions for Merge Intervals.
---
+++ Blind75CSharpTest/Week01/IntervalsTest.cs
@@ -0,0 +1,37 @@
+﻿using Blind75CSharp.Week01;
+using FluentAssertions;
+using Xunit;
+
+namespace Blind75CSharpTest.Week01;
+
+public class IntervalsTest
+{
+   [Fact]
+   public void Intervals_Merge_When_Valid()
+   {
+      {
+         int[][] jaggedArrayIsBad =
+         {
+            new[] {8, 10},
+            new[] {1, 3},
+            new[] {15, 18},
+            new[] {2, 6},
+         };
+
+         var expected = new[]
+         {
+            new[] {1, 6},
+            new[] {8, 10},
+            new[] {15, 18}
+         };
+
+         var testObject = new Intervals();
+         var actuals = testObject.Merge(jaggedArrayIsBad);
+
+         for (var i = 0; i < expected.Length; i++)
+         {
+            actuals[i].Should().Contain(expected[i]);
+         }
+      }
+   }
+}
