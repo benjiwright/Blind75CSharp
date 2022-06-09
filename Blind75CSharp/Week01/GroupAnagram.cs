9010d36bcbae4e9863b4e9fa842fffd56260ed3f
Benji <3194212+benjiwright@users.noreply.github.com>
2022-06-09 09:44:34 -0600

    group anagram without sorting, small improvement

--- Blind75CSharp/Week01/GroupAnagram.cs
+++ Blind75CSharp/Week01/GroupAnagram.cs
@@ -2,7 +2,7 @@
 
 public class GroupAnagram
 {
-   public IList<IList<string>> GroupAnagrams(string[] strs)
+   public IList<IList<string>> GroupAnagramsWithSort(string[] strs)
    {
       var results = new Dictionary<string, List<string>>();
 
@@ -21,8 +21,38 @@ public class GroupAnagram
       }
 
       return results.Values.Cast<IList<string>>().ToList();
+      // Runtime: 329 ms, faster than 17.60% of C# online submissions for Group Anagrams.
+      // Memory Usage: 50.3 MB, less than 73.74% of C# online submissions for Group Anagrams.
+   }
+
+   public IList<IList<string>> GroupAnagrams(string[] strs)
+   {
+      var result = new List<IList<string>>();
+      if (strs.Length == 0) return result;
+      var map = new Dictionary<string, List<string>>();
+
+      foreach (var s in strs)
+      {
+         var hash = new char[26];
+         foreach (var c in s)
+         {
+            hash[c - 'a']++;
+         }
+
+         var key = new string(hash);
+         if (map.ContainsKey(key))
+         {
+            map[key].Add(s);
+         }
+         else
+         {
+            map.Add(key, new List<string> {s});
+         }
+      }
+
+      return map.Values.Cast<IList<string>>().ToList();
+      // without sorting (n log n), very small improvement
+      // Runtime: 301 ms, faster than 26.88% of C# online submissions for Group Anagrams.
+      // Memory Usage: 51.4 MB, less than 36.63% of C# online submissions for Group Anagrams.
    }
 }
-
-// Runtime: 329 ms, faster than 17.60% of C# online submissions for Group Anagrams.
-// Memory Usage: 50.3 MB, less than 73.74% of C# online submissions for Group Anagrams.
---
+++ Blind75CSharpTest/Week01/GroupAnagramTest.cs
@@ -0,0 +1,15 @@
+﻿using Blind75CSharp.Week01;
+using Xunit;
+
+namespace Blind75CSharpTest.Week01;
+
+public class GroupAnagramTest
+{
+   [Fact]
+   public void GroupAnagram_Valid()
+   {
+      var input = new string[] {"eat", "tea", "tan", "ate", "nat", "bat"};
+      var testObject = new GroupAnagram();
+      var actual = testObject.GroupAnagrams(input);
+   }
+}
