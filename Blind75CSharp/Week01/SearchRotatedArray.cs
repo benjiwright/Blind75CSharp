bd032be0708b584fa8d50fc7b61306ca1daf8ab8
Benji <3194212+benjiwright@users.noreply.github.com>
2022-06-13 17:12:57 -0600

    Find Minimum in Rotated Sorted Array

--- Blind75CSharp/Week01/SearchRotatedArray.cs
+++ Blind75CSharp/Week01/SearchRotatedArray.cs
@@ -2,6 +2,7 @@
 
 public class SearchRotatedArray
 {
+   // Find Max
    public int Search(int[] nums, int target)
    {
       if (nums.Length == 0) return -1;
@@ -49,7 +50,52 @@ public class SearchRotatedArray
       // not found
       return -1;
    }
-}
+   // Runtime: 88 ms, faster than 87.03% of C# online submissions for Search in Rotated Sorted Array.
+   // Memory Usage: 38 MB, less than 38.43% of C# online submissions for Search in Rotated Sorted Array.
 
-// Runtime: 88 ms, faster than 87.03% of C# online submissions for Search in Rotated Sorted Array.
-// Memory Usage: 38 MB, less than 38.43% of C# online submissions for Search in Rotated Sorted Array.
+   public int FindMin(int[] nums)
+   {
+      if (nums is null) return -1;
+
+      var left = 0;
+      var right = nums.Length - 1;
+
+      // collection is already sorted
+      if (nums[left] < nums[right]) return nums[0];
+      
+      while (left <= right)
+      {
+         var mid = left + (right - left) / 2;
+         
+         // are we sorted?
+         if (mid == right)
+         {
+            return (nums[mid]);
+         }
+
+         // is this the inflection point on either side?
+         if (nums[mid] > nums[mid + 1])
+         {
+            return nums[mid + 1];
+         }
+         else if(nums[mid-1] > nums[mid])
+         {
+            return nums[mid];
+         }
+
+         // which side do we continue the search?
+         if (nums[left] < nums[mid])
+         {
+            left = mid + 1;
+         }
+         else
+         {
+            right = mid - 1;
+         }
+      }
+
+      return -1;
+   }
+   // Runtime: 121 ms, faster than 38.25% of C# online submissions for Find Minimum in Rotated Sorted Array.
+   // Memory Usage: 38.1 MB, less than 37.44% of C# online submissions for Find Minimum in Rotated Sorted Array.
+}
--- Blind75CSharpTest/Week01/SearchRotatedArrayTest.cs
+++ Blind75CSharpTest/Week01/SearchRotatedArrayTest.cs
@@ -15,4 +15,33 @@ public class SearchRotatedArrayTest
       var actual = testObject.Search(input, target);
       actual.Should().Be(4);
    }
+
+   [Fact]
+   public void SearchRotatedArray_FindMin_When_Valid()
+   {
+      var input = new int[] {3,4,5,1,2};
+      var testObject = new SearchRotatedArray();
+      var actual = testObject.FindMin(input);
+      actual.Should().Be(1);
+   }
+   
+   [Fact]
+   public void SearchRotatedArray_FindMin_When_Valid_Sorted()
+   {
+      var input = new int[] { 11,13,15,17};
+      var testObject = new SearchRotatedArray();
+      var actual = testObject.FindMin(input);
+      actual.Should().Be(11);
+   }
+   
+   
+   [Fact]
+   public void SearchRotatedArray_FindMin_When_Valid_Again()
+   {
+      var input = new int[] { 5,1,2,3,4};
+      var testObject = new SearchRotatedArray();
+      var actual = testObject.FindMin(input);
+      actual.Should().Be(1);
+   }
+  
 }
