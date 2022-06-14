801e8b13a89e4764697356620e73b4728d8b9900
Benji <3194212+benjiwright@users.noreply.github.com>
2022-06-14 09:58:07 -0600

    424. Longest Repeating Character Replacement

---
+++ Blind75CSharp/Week02/LetterReplacement.cs
@@ -0,0 +1,38 @@
+﻿namespace Blind75CSharp.Week02;
+
+public class LetterReplacement
+{
+   public int CharacterReplacement(string s, int k)
+   {
+      if (s == null || s.Length == 0)
+         return 0;
+
+      var charCounts = new Dictionary<char, int>();
+
+      int left = 0, right = 0, maxCnt = 0, maxLen = 0;
+
+      while (right < s.Length)
+      {
+         var rightChar = s[right];
+         if (charCounts.ContainsKey(rightChar))
+            charCounts[rightChar]++;
+         else
+            charCounts.Add(rightChar, 1);
+         right++;
+
+         maxCnt = Math.Max(maxCnt, charCounts[rightChar]);
+
+         if (right - left - maxCnt > k)
+         {
+            var leftChar = s[left];
+            charCounts[leftChar]--;
+            left++;
+         }
+
+         maxLen = Math.Max(maxLen, right - left);
+      }
+      return maxLen;
+   }
+   // Runtime: 96 ms, faster than 62.24% of C# online submissions for Longest Repeating Character Replacement.
+   // Memory Usage: 35.8 MB, less than 94.13% of C# online submissions for Longest Repeating Character Replacement.
+}
---
+++ Blind75CSharpTest/Week02/LetterReplacementTest.cs
@@ -0,0 +1,19 @@
+﻿using Blind75CSharp.Week02;
+using FluentAssertions;
+using Xunit;
+
+namespace Blind75CSharpTest.Week02;
+
+public class LetterReplacementTest
+{
+   [Theory]
+   [InlineData("ABAB", 2, 4)]
+   [InlineData("AABABBA", 1, 4)]
+   [InlineData("AAAA", 2, 4)]
+   public void CharacterReplacement_Valid_When_ReplacementsLessThanSize(string input, int replacements, int expected)
+   {
+      var testObject = new LetterReplacement();
+      var actual = testObject.CharacterReplacement(input, replacements);
+      actual.Should().Be(expected); //                
+   }
+}
