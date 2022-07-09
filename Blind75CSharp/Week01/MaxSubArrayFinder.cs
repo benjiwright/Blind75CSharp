94b3f8f56f560502063f44853fdf922f139d1152
Benji <3194212+benjiwright@users.noreply.github.com>
2022-07-09 09:17:27 -0600

    152. Maximum Product Subarray

--- Blind75CSharp/Week01/MaxSubArrayFinder.cs
+++ Blind75CSharp/Week01/MaxSubArrayFinder.cs
@@ -2,16 +2,42 @@
 
 public class MaxSubArrayFinder
 {
+   // 53 https://leetcode.com/problems/maximum-subarray/
    public int MaxSubArray(int[] nums)
    {
       int maxSum = nums[0], currMax = nums[0];
 
-      for (var i = 1; i < nums.Length; i++)
+      for (var idx = 1; idx < nums.Length; idx++)
       {
-         currMax = Math.Max(currMax, currMax + nums[i]);
+         // does adding this index improve our current max?
+         currMax = Math.Max(nums[idx], currMax + nums[idx]);
+
+         // do we update the overall max?
          if (currMax > maxSum) maxSum = currMax;
       }
 
       return maxSum;
    }
+   // Runtime: 396 ms, faster than 12.27% of C# online submissions for Maximum Subarray.
+   // Memory Usage: 49 MB, less than 43.02% of C# online submissions for Maximum Subarray.
+
+   // 152 https://leetcode.com/problems/maximum-product-subarray/
+   public int MaxProduct(int[] nums)
+   {
+      var result = nums.Max();
+      var max = 1;
+      var min = 1; // gotcha 1 - gotta track the min product
+
+      foreach (var num in nums)
+      {
+         var tmp = max * num; // gotcha 2 - need to use old max before computing min 
+         max = new[] {tmp, min * num, num}.Max();
+         min = new[] {tmp, min * num, num}.Min();
+         result = Math.Max(result, max);
+      }
+
+      return result;
+   }
+   // Runtime: 158 ms, faster than 19.58% of C# online submissions for Maximum Product Subarray.
+   // Memory Usage: 39.3 MB, less than 23.29% of C# online submissions for Maximum Product Subarray.
 }
--- Blind75CSharpTest/Week01/MaxSubArrayFinderTest.cs
+++ Blind75CSharpTest/Week01/MaxSubArrayFinderTest.cs
@@ -6,15 +6,25 @@ namespace Blind75CSharpTest.Week01;
 
 public class MaxSubArrayFinderTest
 {
-   [Fact]
-   public void MaxSubArrayFinderTest_When_Valid()
+   [Theory]
+   [InlineData(new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4}, 6)]
+   public void MaxSubArray_When_Valid(int[] input, int expected)
    {
-      var input = new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4};
       var testObject = new MaxSubArrayFinder();
       var actual = testObject.MaxSubArray(input);
 
-      // TODO: skipped bonus question from week 01
-      
-      // actual.Should().Be(6);
+      actual.Should().Be(expected);
+   }
+
+   [Theory]
+   [InlineData(new int[] {2, 3, -2, 4}, 6)]
+   [InlineData(new int[] {-1, 8}, 8)]
+   [InlineData(new int[] {-2, 3, -4}, 24)]
+   public void MaxProduct_When_Valid(int[] input, int expected)
+   {
+      var testObject = new MaxSubArrayFinder();
+      var actual = testObject.MaxProduct(input);
+
+      actual.Should().Be(expected);
    }
 }
