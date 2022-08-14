3ceabc7c7b530f6f3afdc2d27fe350afcf328308
Benji <3194212+benjiwright@users.noreply.github.com>
2022-08-14 12:24:31 -0600

    929. Unique Email Addresses

--- Blind75CSharp/Week06/Solution06.cs
+++ Blind75CSharp/Week06/Solution06.cs
@@ -4,6 +4,85 @@ namespace Blind75CSharp.Week06;
 
 public class Solution06
 {
+   public int NumUniqueEmails(string[] emails)
+   {
+      var uniqueEmail = new HashSet<string>();
+
+      foreach (var email in emails)
+      {
+         var local = new StringBuilder();
+         for (var idx = 0; idx < email.Length; idx++)
+         {
+            switch (email[idx])
+            {
+               case '.':
+                  continue;
+               case '+':
+               {
+                  while (email[idx] != '@')
+                  {
+                     idx++;
+                  }
+
+                  break; // switch, not the 'for'
+               }
+            }
+
+            if (email[idx] == '@')
+            {
+               var domain = email.Substring(idx);
+               var parsedEmail = local + domain;
+               uniqueEmail.Add(parsedEmail);
+               break;
+            }
+
+            local.Append(email[idx]);
+         }
+      }
+
+      return uniqueEmail.Count;
+   }
+   // Runtime: 139 ms, faster than 53.54% of C# online submissions for Unique Email Addresses.
+   // Memory Usage: 39.4 MB, less than 88.33% of C# online submissions for Unique Email Addresses.
+
+   public IList<string> LetterCombinations(string digits)
+   {
+      var results = new List<StringBuilder>();
+
+      if (digits is null || digits.Length == 0) return new List<string>();
+
+      var map = new Dictionary<char, List<char>>
+      {
+         {'2', new List<char> {'a', 'b', 'c'}},
+         {'3', new List<char> {'d', 'e', 'f'}},
+         {'4', new List<char> {'g', 'h', 'i'}},
+         {'5', new List<char> {'j', 'k', 'l'}},
+         {'6', new List<char> {'m', 'n', 'o'}},
+         {'7', new List<char> {'p', 'q', 'r', 's'}},
+         {'8', new List<char> {'t', 'u', 'v'}},
+         {'9', new List<char> {'w', 'x', 'y', 'z'}},
+      };
+
+      for (var idx = 0; idx < digits.Length; idx++)
+      {
+         var digit = digits[idx];
+         var chars = map[digit]; // trust data, instead of TryGetValue?
+
+         for (var j = 0; j < chars.Count; j++)
+         {
+            var c = chars[j];
+            for (var k = 0; k < chars.Count; k++)
+            {
+               results.Add(new StringBuilder());
+               var index = j + k;
+               results[index].Append(c);
+            }
+         }
+      }
+
+      return results.Select(result => result.ToString()).ToList();
+   }
+
    public int RomanToInt(string s)
    {
       var map = new Dictionary<char, int>
