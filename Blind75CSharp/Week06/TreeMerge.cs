7fe4990d2e4633c09a9a49c8d2825d8ecce6d549
Benji Wright <3194212+benjiwright@users.noreply.github.com>
2022-09-06 16:30:14 -0600

    617. Merge Two Binary Trees

---
+++ Blind75CSharp/Week06/TreeMerge.cs
@@ -0,0 +1,34 @@
+﻿namespace Blind75CSharp.Week06;
+
+public class TreeMerge
+{
+   public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+   {
+      if (root1 is null && root2 is null) return null;
+
+      var left = root1?.val ?? 0;
+      var right = root2?.val ?? 0;
+
+      var combined = new TreeNode(left + right);
+      combined.left = MergeTrees(root1?.left, root2?.left);
+      combined.right = MergeTrees(root1?.right, root2?.right);
+
+      return combined;
+   }
+}
+// Runtime: 209 ms, faster than 7.42% of C# online submissions for Merge Two Binary Trees.
+// Memory Usage: 40.1 MB, less than 16.86% of C# online submissions for Merge Two Binary Trees.
+
+public class TreeNode
+{
+   public int val;
+   public TreeNode left;
+   public TreeNode right;
+
+   public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+   {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+   }
+}
---
+++ Blind75CSharpTest/Week06/TreeMergeTest.cs
@@ -0,0 +1,32 @@
+﻿using Blind75CSharp.Week06;
+using Xunit;
+
+namespace Blind75CSharpTest.Week06;
+
+public class TreeMergeTest
+{
+
+   [Fact]
+   public void MergeTrees()
+   {
+      var one = new TreeNode(1);
+      one.left = new TreeNode(3);
+      one.right = new TreeNode(2);
+      one.left.left = new TreeNode(5);
+
+      var two = new TreeNode(2);
+      two.left = new TreeNode(1);
+      two.right = new TreeNode(3);
+      two.left.right = new TreeNode(4);
+      two.right.right = new TreeNode(7);
+
+
+      var testObj = new TreeMerge();
+      testObj.MergeTrees(one, two);
+
+
+
+
+   }
+
+}
