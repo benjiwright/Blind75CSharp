f2358d347cdf2f425b0c477f1ac5103258848627
Benji Wright <3194212+benjiwright@users.noreply.github.com>
2022-09-08 11:47:47 -0600

    778. Swim in Rising Water

---
+++ Blind75CSharp/Week06/Swim.cs
@@ -0,0 +1,49 @@
+﻿namespace Blind75CSharp.Week06;
+
+public class Swim
+{
+   public int SwimInWater(int[][] grid)
+   {
+      var n = grid.Length;
+      var directions = new List<(int, int)>
+      {
+         (0, 1),
+         (0, -1),
+         (1, 0),
+         (-1, 0),
+      };
+
+      var frontier = new PriorityQueue<(int, int), int>(); // (r,c)
+      frontier.Enqueue((0, 0), grid[0][0]);
+      var visited = new HashSet<(int, int)> {(0, 0)};
+
+      while (frontier.Count > 0)
+      {
+         frontier.TryDequeue(out var tup, out var weight);
+         var (r, c) = tup;
+
+         if (r == n - 1 && c == n - 1) return weight;
+
+         // add the frontier for my location
+         foreach (var (x, y) in directions)
+         {
+            var newRow = r + x;
+            var newCol = c + y;
+
+            if (newRow < 0 || newRow == n ||
+                newCol < 0 || newCol == n ||
+                visited.Contains((newRow, newCol)))
+               continue;
+
+            visited.Add((newRow, newCol));
+
+            var maxToGetHere = Math.Max(grid[newRow][newCol], weight);
+            frontier.Enqueue((newRow, newCol), maxToGetHere);
+         }
+      }
+
+      return -1;
+   }
+   // Runtime: 189 ms, faster than 22.35% of C# online submissions for Swim in Rising Water.
+   // Memory Usage: 40.2 MB, less than 32.94% of C# online submissions for Swim in Rising Water.
+}
---
+++ Blind75CSharpTest/Week06/SwimTest.cs
@@ -0,0 +1,37 @@
+﻿using Blind75CSharp.Week06;
+using FluentAssertions;
+using Xunit;
+
+namespace Blind75CSharpTest.Week06;
+
+public class SwimTest
+{
+   [Fact]
+   public void LetsSwimInTheDeepEnd()
+   {
+      var grid = new[]
+      {
+         new[] {00, 01, 02, 03, 04},
+         new[] {24, 23, 22, 21, 05},
+         new[] {12, 13, 14, 15, 16},
+         new[] {11, 17, 18, 19, 20},
+         new[] {10, 09, 08, 07, 06},
+      };
+
+      var testObj = new Swim();
+      testObj.SwimInWater(grid).Should().Be(16);
+   }
+
+   [Fact]
+   public void EasyMode()
+   {
+      var grid = new[]
+      {
+         new[] {3, 2},
+         new[] {0, 1},
+      };
+
+      var testObj = new Swim();
+      testObj.SwimInWater(grid).Should().Be(3);
+   }
+}
