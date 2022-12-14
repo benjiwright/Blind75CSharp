21540ddd32911fed576323b280ba87b55def0fc4
Benji Wright <3194212+benjiwright@users.noreply.github.com>
2022-12-14 07:08:45 -0700

    973. K Closest Points to Origin

---
+++ Blind75CSharp/Xero/XeroSolver.cs
@@ -0,0 +1,34 @@
+﻿namespace Blind75CSharp.Xero;
+
+public class XeroSolver
+{
+   // Runtime 348 ms Beats 93.89%
+   // Memory 58.1 MB Beats 15.2%
+   // https://leetcode.com/problems/k-closest-points-to-origin/submissions/859405986/
+   public int[][] KClosest(int[][] points, int k)
+   {
+      var pq = new PriorityQueue<int[], double>();
+      foreach (var a in points)
+      {
+         // i hate jagged arrays
+         var distance = FindDistanceToOrigin(a[0], a[1]);
+
+         pq.Enqueue(a, distance);
+      }
+
+      var result = new List<int[]>();
+
+      while (k > 0)
+      {
+         result.Add(pq.Dequeue());
+         k--;
+      }
+
+      return result.ToArray();
+   }
+
+   private static double FindDistanceToOrigin(int x, int y)
+   {
+      return Math.Sqrt(x * x + y * y);
+   }
+}
---
+++ Blind75CSharpTest/ZeroTest/XeroSovlerTest.cs
@@ -0,0 +1,15 @@
+﻿using Blind75CSharp.Xero;
+using FluentAssertions;
+using Xunit;
+
+namespace Blind75CSharpTest.ZeroTest;
+
+public class XeroSovlerTest
+{
+   [Theory]
+   [InlineData(new int[] {2, 1, 2}, 5)]
+   public void FindDistanceToOrigin(int[] input, int expected)
+   {
+      var testObj = new XeroSolver();
+   }
+}
