506cf5e7af5c1d38a1bd0ee9da73114cea4bcf49
Benji Wright <3194212+benjiwright@users.noreply.github.com>
2023-10-07 10:49:24 -0600

    Advent of Code 2022. Day 6

---
+++ AdventOfCode/Year2022/AoC2022Day06.cs
@@ -0,0 +1,42 @@
+﻿namespace AdventOfCode.Year2022;
+
+public class AoC2022Day06
+{
+   public int Part01And02(string line, int size = 4)
+   {
+      int windowSize = size;
+      var right = windowSize - 1;
+      var dict = new Dictionary<char,int>();
+      
+      for (var i = 0; i < windowSize; i++)
+      {
+         var c = line[i];
+         dict.TryAdd(c, 0);
+         dict[c]++;
+      }
+      
+      if (dict.Count == windowSize) return right + 1;
+
+      // iterate
+      for(var left=0; right<line.Length; left++)
+      {
+         if (dict.Count == windowSize) return right + 1;
+
+         var c = line[left];
+         dict[c]--;
+         if (dict[c] == 0) dict.Remove(c);
+         
+
+         if (right + 1 == line.Length) return -1;
+         right++;
+         
+         if(!dict.ContainsKey(line[right]))
+            dict.Add(line[right],0);
+
+         dict[line[right]]++;
+
+      }
+      
+      return -1;
+   }
+}
--- AdventOfCodeTest/SolverTests.cs
+++ AdventOfCodeTest/SolverTests.cs
@@ -164,4 +164,44 @@ public class SolverTests
       var actual = sut.Part02(lines);
       actual.Should().Be(expected);
    }
+
+   [Theory]
+   [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz",5)]
+   [InlineData("nppdvjthqldpwncqszvftbrmjlhg",6)]
+   [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",10)]
+   [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",11)]
+   public void Day06_Part01_SmallData(string input, int expected)
+   {
+      var sut = new AoC2022Day06();
+      var actual = sut.Part01And02(input);
+      actual.Should().Be(expected);
+   }
+
+   [Fact]
+   public void Day06_Part01()
+   {
+      const string day = "06";
+      const string part = "01";
+      const int expected = 1142;
+
+      var fileName = @$"Year2022\inputs\Day{day}-Part{part}.txt";
+      var lines = FileReader.ReadAllLines(fileName);
+      var sut = new AoC2022Day06();
+      var actual = sut.Part01And02(lines[0]);
+      actual.Should().Be(expected);
+   }
+
+   [Fact]
+   public void Day06_Part02()
+   {
+      const string day = "06";
+      const string part = "02";
+      const int expected = 2803;
+
+      var fileName = @$"Year2022\inputs\Day{day}-Part{part}.txt";
+      var lines = FileReader.ReadAllLines(fileName);
+      var sut = new AoC2022Day06();
+      var actual = sut.Part01And02(lines[0],14);
+      actual.Should().Be(expected);
+   }
 }
