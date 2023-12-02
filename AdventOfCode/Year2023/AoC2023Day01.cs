869e970307c36973aaf632833e7fb615ab86850a
Benji Wright <3194212+benjiwright@users.noreply.github.com>
2023-12-02 09:46:59 -0700

    welcome to AoC 2023. Day 1 solved

--- AdventOfCode/Year2023/AoC2023Day01.cs
+++ AdventOfCode/Year2023/AoC2023Day01.cs
@@ -1,17 +1,116 @@
-using AdventOfCode.Utils;
-
 namespace AdventOfCode.Year2023;
 
 // https://adventofcode.com/2023/day/1/
 public class AoC2023Day01
 {
-    public int Part01(string[] lines)
-    {
-        return -1;
-    }
+   public int Part01(string[] lines)
+   {
+      var sum = 0;
 
-    public int Part02(string[] lines)
-    {
-        return -1;
-    }
+      foreach (var line in lines)
+      {
+         sum += FindLineScorePart01(line);
+      }
+
+      return sum;
+   }
+
+   public int Part02(string[] lines)
+   {
+      var numbers = new Dictionary<string, int>
+      {
+         {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8},
+         {"nine", 9},
+      };
+
+      int sum = 0;
+
+      foreach (var line in lines)
+      {
+         int first = FindFirst(line, numbers);
+         int second = FindSecond(line, numbers);
+
+         sum += first * 10 + second;
+      }
+
+      return sum;
+   }
+
+   private int FindSecond(string line, Dictionary<string, int> numbers)
+   {
+      for (int i = line.Length - 1; i >= 0; i--)
+      {
+         var c = line[i];
+         if (char.IsDigit(c)) return int.Parse(c.ToString());
+
+         if (char.IsLetter(c))
+         {
+            try
+            {
+               for (int numSize = 3; numSize <= 5; numSize++)
+               {
+                  var comparing = line.Substring(i - numSize + 1, numSize);
+                  if (numbers.TryGetValue(comparing, out var value)) return value;
+               }
+            }
+            catch (Exception e)
+            {
+            }
+         }
+      }
+
+      throw new ArgumentException("did not find the second number");
+   }
+
+   private int FindFirst(string line, Dictionary<string, int> numbers)
+   {
+      for (var i = 0; i < line.Length; i++)
+      {
+         var c = line[i];
+         if (char.IsDigit(c)) return int.Parse(c.ToString());
+
+         if (char.IsLetter(c))
+         {
+            try
+            {
+               for (int numSize = 3; numSize <= 5; numSize++)
+               {
+                  var comparing = line.Substring(i, numSize);
+                  if (numbers.TryGetValue(comparing, out var value)) return value;
+               }
+            }
+            catch (Exception e)
+            {
+            }
+         }
+      }
+
+      throw new ArgumentException("did not find the first number");
+   }
+
+
+   private int FindLineScorePart01(string s)
+   {
+      char firstDigit = '\n', secondDigit = '\n';
+
+      foreach (var c in s)
+      {
+         if (char.IsDigit(c))
+         {
+            firstDigit = c;
+            break;
+         }
+      }
+
+      for (int i = s.Length - 1; i >= 0; i--)
+      {
+         if (char.IsDigit(s[i]))
+         {
+            secondDigit = s[i];
+            break;
+         }
+      }
+
+      return int.Parse(firstDigit + secondDigit.ToString());
+   }
 }
--- AdventOfCodeTest/Year2023/Aoc2023Tester.cs
+++ AdventOfCodeTest/Year2023/Aoc2023Tester.cs
@@ -2,7 +2,7 @@ using AdventOfCode.Utils;
 using AdventOfCode.Year2023;
 using FluentAssertions;
 
-namespace AdventOfCodeTest;
+namespace AdventOfCodeTest.Year2023;
 
 public class Aoc2023Tester
 {
@@ -35,24 +35,23 @@ public class Aoc2023Tester
     [Fact]
     public void Day01_Part01()
     {
-        const string day = "XXX";
+        const string day = "01";
         const string part = "01";
-        const int expected = -1;
-
-        var fileName = @$"Year2023\inputs\2023\Day{day}-Part{part}.txt";
+        const int expected = 56397;
+        var fileName = @$"Year2023\inputs\Day{day}-Part{part}.txt";
         var lines = FileReader.ReadAllLines(fileName);
         AoC2023Day01 sut = new();
         sut.Part01(lines).Should().Be(expected);
     }
 
-    [Fact(Skip = "copy/paste boiler plate")]
+    [Fact]
     public void Day01_Part2()
     {
-        const string day = "XXX";
+        const string day = "01";
         const string part = "02";
-        const int expected = -1;
+        const int expected = 55701;
 
-        var fileName = @$"Year2023\inputs\2023\Day{day}-Part{part}.txt";
+        var fileName = @$"Year2023\inputs\Day{day}-Part{part}.txt";
         var lines = FileReader.ReadAllLines(fileName);
         AoC2023Day01 sut = new();
         sut.Part02(lines).Should().Be(expected);
