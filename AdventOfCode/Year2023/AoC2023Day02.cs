cce761237d5ac259de3e2b37d085b27b73f8e5b5
Benji Wright <3194212+benjiwright@users.noreply.github.com>
2023-12-02 11:54:22 -0700

    AoC 2023 Day 02 - let's go!

---
+++ AdventOfCode/Year2023/AoC2023Day02.cs
@@ -0,0 +1,120 @@
+﻿using System.Text;
+
+namespace AdventOfCode.Year2023;
+
+public class AoC2023Day02
+{
+   public int Part01(string[] lines, int red, int green, int blue)
+   {
+      var result = 0;
+      foreach (var line in lines)
+      {
+         if (IsPossible(line, red, blue, green))
+         {
+            result += ParseGameLineForGameNumber(line);
+         }
+      }
+
+      return result;
+   }
+
+   public int Part02(string[] lines)
+   {
+      var result = 0;
+      foreach (var fullLine in lines)
+      {
+         var lineWithOutGameNumber = fullLine.Split(':')[1];
+         result += FindLineMax(lineWithOutGameNumber);
+      }
+
+      return result;
+   }
+
+   private int FindLineMax(string lineWithOutGameNumber)
+   {
+      var dict = new Dictionary<string, int>();
+
+      foreach (var grab in lineWithOutGameNumber.Split(';'))
+      {
+         var numWithColor = grab.Split(',');
+
+         foreach (var s in numWithColor)
+         {
+            var count = GetCount(s);
+            var color = GetColor(s);
+
+            dict.TryAdd(color, count);
+            var prevMax = dict[color];
+            dict[color] = Math.Max(prevMax, count);
+         }
+      }
+
+      return dict.Values.Aggregate(1, (acc, val) => acc * val);
+   }
+
+   private bool IsPossible(string fullLine, int red, int blue, int green)
+   {
+      var line = fullLine.Split(':')[1];
+
+      var grabs = line.Split(';');
+
+      var limits = new Dictionary<string, int>
+      {
+         {"red", red},
+         {"blue", blue},
+         {"green", green},
+      };
+
+      foreach (var grab in grabs)
+      {
+         var colors = grab.Split(',');
+
+         foreach (var s in colors)
+         {
+            var count = GetCount(s);
+            string color = GetColor(s);
+
+            if (!limits.TryGetValue(color, out int maxColor))
+               throw new ArgumentException($"color {color} not found");
+
+            if (maxColor < count) return false;
+         }
+      }
+
+      return true;
+   }
+
+   private string GetColor(string s)
+   {
+      var color = s.Trim().Split(' ')[1];
+      return color.Trim();
+   }
+
+   private int GetCount(string s)
+   {
+      var num = s.Trim().Split(' ')[0];
+      return int.Parse(num);
+   }
+
+   private int ParseGameLineForGameNumber(string line)
+   {
+      var words = line.Split(':');
+      var word = words[0];
+
+      var i = word.Length - 1;
+      var reverseNumber = new StringBuilder();
+      while (char.IsDigit(word[i]))
+      {
+         reverseNumber.Append(word[i]);
+         i--;
+      }
+
+      var numberAsSb = new StringBuilder();
+      foreach (var c in reverseNumber.ToString().Reverse())
+      {
+         numberAsSb.Append(c);
+      }
+
+      return int.Parse(numberAsSb.ToString());
+   }
+}
--- AdventOfCodeTest/Year2023/Aoc2023Tester.cs
+++ AdventOfCodeTest/Year2023/Aoc2023Tester.cs
@@ -6,54 +6,85 @@ namespace AdventOfCodeTest.Year2023;
 
 public class Aoc2023Tester
 {
-    [Fact(Skip = "copy/paste boiler plate")]
-    public void DayXX_Part01()
-    {
-        const string day = "XXX";
-        const string part = "01";
-        const int expected = -1;
+   [Fact(Skip = "copy/paste boiler plate")]
+   public void DayXX_Part01()
+   {
+      const string day = "XXX";
+      const string part = "01";
+      const int expected = -1;
 
-        var fileName = @$"Year2023\inputs\2023\Day{day}-Part{part}.txt";
-        var lines = FileReader.ReadAllLines(fileName);
-        AoC2023Day01 sut = new();
-        sut.Part01(lines).Should().Be(expected);
-    }
+      var fileName = @$"Year2023\inputs\Day{day}-Part{part}.txt";
+      var lines = FileReader.ReadAllLines(fileName);
+      AoC2023Day01 sut = new();
+      sut.Part01(lines).Should().Be(expected);
+   }
 
-    [Fact(Skip = "copy/paste boiler plate")]
-    public void DayXX_Part02()
-    {
-        const string day = "XXX";
-        const string part = "02";
-        const int expected = -1;
+   [Fact(Skip = "copy/paste boiler plate")]
+   public void DayXX_Part02()
+   {
+      const string day = "XXX";
+      const string part = "02";
+      const int expected = -1;
 
-        var fileName = @$"Year2023\inputs\2023\Day{day}-Part{part}.txt";
-        var lines = FileReader.ReadAllLines(fileName);
-        AoC2023Day01 sut = new();
-        sut.Part02(lines).Should().Be(expected);
-    }
+      var fileName = @$"Year2023\inputs\Day{day}-Part{part}.txt";
+      var lines = FileReader.ReadAllLines(fileName);
+      AoC2023Day01 sut = new();
+      sut.Part02(lines).Should().Be(expected);
+   }
 
-    [Fact]
-    public void Day01_Part01()
-    {
-        const string day = "01";
-        const string part = "01";
-        const int expected = 56397;
-        var fileName = @$"Year2023\inputs\Day{day}-Part{part}.txt";
-        var lines = FileReader.ReadAllLines(fileName);
-        AoC2023Day01 sut = new();
-        sut.Part01(lines).Should().Be(expected);
-    }
+   [Fact]
+   public void Day01_Part01()
+   {
+      const string day = "01";
+      const string part = "01";
+      const int expected = 56397;
+      var fileName = @$"Year2023\inputs\Day{day}-Part{part}.txt";
+      var lines = FileReader.ReadAllLines(fileName);
+      AoC2023Day01 sut = new();
+      sut.Part01(lines).Should().Be(expected);
+   }
 
-    [Fact]
-    public void Day01_Part2()
-    {
-        const string day = "01";
-        const string part = "02";
-        const int expected = 55701;
+   [Fact]
+   public void Day01_Part2()
+   {
+      const string day = "01";
+      const string part = "02";
+      const int expected = 55701;
 
-        var fileName = @$"Year2023\inputs\Day{day}-Part{part}.txt";
-        var lines = FileReader.ReadAllLines(fileName);
-        AoC2023Day01 sut = new();
-        sut.Part02(lines).Should().Be(expected);
-    }
+      var fileName = @$"Year2023\inputs\Day{day}-Part{part}.txt";
+      var lines = FileReader.ReadAllLines(fileName);
+      AoC2023Day01 sut = new();
+      sut.Part02(lines).Should().Be(expected);
+   }
+
+
+   [Fact]
+   public void Day02_Part01()
+   {
+      const string day = "02";
+      const string part = "01";
+      const int expected = 2679;
+
+      int red = 12;
+      int green = 13;
+      int blue = 14;
+
+      var fileName = @$"Year2023\inputs\Day{day}-Part{part}.txt";
+      var lines = FileReader.ReadAllLines(fileName);
+      AoC2023Day02 sut = new();
+      sut.Part01(lines,red,green,blue).Should().Be(expected);
+   }
+
+   [Fact]
+   public void Day02_Part02()
+   {
+      const string day = "02";
+      const string part = "02";
+      const int expected = 77607;
+
+      var fileName = @$"Year2023\inputs\Day{day}-Part{part}.txt";
+      var lines = FileReader.ReadAllLines(fileName);
+      AoC2023Day02 sut = new();
+      sut.Part02(lines).Should().Be(expected);
+   }
 }
