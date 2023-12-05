4d95b405d352051562ddbbb9374d4c733e70baa9
Benji Wright <3194212+benjiwright@users.noreply.github.com>
2023-12-04 21:27:24 -0700

    AoC 2023 Day 4 Part 2 - After work party

--- AdventOfCode/Year2023/AoC2023Day04.cs
+++ AdventOfCode/Year2023/AoC2023Day04.cs
@@ -42,8 +42,49 @@ public class AoC2023Day04
       // 5 => 16
    }
 
+   private int[] array;
+
    public int Part02(string[] lines)
    {
-      return -2;
+      array = new int [lines.Length];
+      Array.Fill(array, 1);
+
+      foreach (var line in lines)
+      {
+         var scores = ParseScoreCardLinePart2(line);
+         SimulatePlay(scores.gameId, scores.matches);
+      }
+
+      return array.Sum();
+   }
+
+   private void SimulatePlay(int gameId, int matches)
+   {
+      var multiplier = array[gameId];
+      gameId++;
+      for (; gameId < array.Length && matches > 0; gameId++)
+      {
+         array[gameId]+= multiplier;
+         matches--;
+      }
+   }
+
+   private (int gameId, int matches) ParseScoreCardLinePart2(string line)
+   {
+      // Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+      var fullSplit = line.Split(":");
+      var game = int.Parse(fullSplit[0].Split("Card ")[1].Trim()) - 1;
+      var split = fullSplit[1].Split('|');
+      var winning = split[0].Split(' ');
+      var picked = split[1].Split(' ');
+
+      var intersect = winning.Intersect(picked).ToList();
+      var count = intersect.Count();
+      if (intersect.Contains(string.Empty)) // could trim with calling select on both collections
+      {
+         count--;
+      }
+
+      return (game, count);
    }
 }
--- AdventOfCodeTest/Year2023/Aoc2023Tester.cs
+++ AdventOfCodeTest/Year2023/Aoc2023Tester.cs
@@ -124,4 +124,16 @@ public class Aoc2023Tester
       AoC2023Day04 sut = new();
       sut.Part01(lines).Should().Be(expected);
    }
+   
+   [Fact]
+   public void Day04_Part02()
+   {
+      const string day = "04";
+      const string part = "02";
+      const int expected = 7_013_204;
+      var fileName = @$"Year2023\inputs\Day{day}-Part{part}.txt";
+      var lines = FileReader.ReadAllLines(fileName);
+      AoC2023Day04 sut = new();
+      sut.Part02(lines).Should().Be(expected);
+   }
 }
